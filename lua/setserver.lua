01b74c2d1fb8a01f01f81972f5b11d9c4a9435c1
root <root@sclif-desktop>
2015-03-26 01:10:06 +0500

    Исправил setserver

--- lua/setserver.lua
+++ lua/setserver.lua
@@ -8,21 +8,24 @@ local ltn12 = require"ltn12"
 
 node_server = "http://127.0.0.1:8888" -- адрес node.js сервера
 
---local respbody = {}
 local reqbody =""
 
---local cause = "NORMAL_CLEARING"
-
 local ipt = session:getVariable("dialed_ip")
-local cause = session:getVariable("bridge_hangup_cause")
+local cause = session:getVariable("hangup_cause")
+
+
+--dat = env:serialize()            
+--freeswitch.consoleLog("INFO","Here's everything:\n" .. dat .. "\n")
 
 session:consoleLog("info",string.format("Tserver=%s",ipt));
 session:consoleLog("info",string.format("Cause=%s",cause));
 
-if not cause then cause="FAIL" end
+if ipt then
+    if not cause then cause="FAIL" end
 
-reqbody = "ip=" .. ipt .."&cause=" .. cause
-b, c, h = http.request {
+    reqbody = "ip=" .. ipt .."&cause=" .. cause
+    b, c, h = http.request {
           method = "POST",
           url = node_server .. "/set-call?" .. reqbody,
-}
+    }
+end
